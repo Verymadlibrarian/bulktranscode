fb0952599244ea5b79cef4e8d004987bdd1a8a52
Mad <mad@madlibrarian.me>
2025-02-27 00:07:33 +0100

    input_origin_dest

--- main.py
+++ main.py
@@ -2,29 +2,39 @@ import subprocess
 import os
 
 def recursive():
-
+    actual_dir = os.getcwd()
     folders = get_folders()
-    if folders != []:
-        for folder in folders:
+
+    final_dir = destination+actual_dir.replace(initial_dir, '') #Gets the destination + the actual directory structure
+    os.mkdir(final_dir)
+
+    if folders:
+        
+        for folder in folders: #For every folder, get into it and recursively call the function
             os.chdir(folder)
             recursive()
             os.chdir('..')
-    else:
-        place = os.getcwd()
-        files = next(os.walk(place))[2]
+    else: #If there are no folders, get the files and convert them
+        files = get_files()
+        
         for file in files:
-            if file.endswith('.flac') and file.replace('.flac', '.opus') not in files:
-                subprocess.run(['ffmpeg', "-i", place+"\\"+file, "-map", "0:0", "-c:a", "copy", "-acodec", "libopus", file.replace('.flac', '.opus')])
+            if file.endswith(init_extension) and file.replace(init_extension, final_extension) not in files:
+
+                subprocess.run(['ffmpeg', "-i", file, "-map", "0:0","-acodec", "libopus", final_dir+"\\"+file.replace(init_extension, final_extension)])
+
 
 def get_folders():
     return next(os.walk('.'))[1]
 
+def get_files():
+    return next(os.walk('.'))[2]
+
 if __name__ == "__main__":
-    initial_dir = os.getcwd()
+    initial_dir = (input("Source folder :"))
+    initial_dir = fr"{initial_dir}"
+    destination = (input("Source folder :"))
+    destination = fr"{destination}"
+    os.chdir(initial_dir)
+    init_extension = '.flac'
+    final_extension = '.opus'
     recursive()
-
-
-
-
-
-
